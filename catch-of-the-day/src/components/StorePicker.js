dff2c9b0b2a8cba5ff8cde7a078e21b58a4d6eaa
Francisca Vargas <22853887+franciscavargas@users.noreply.github.com>
2019-05-26 16:51:14 +0100

    Chapter 8-13

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,17 +1,37 @@
-import React, { Fragment } from 'react';
+import React, { Fragment } from "react";
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
-    render() {
-        return (
-            <form className="store-selector">
-                { /* This is a comment */}
+  myInput = React.createRef();
 
-                <h2>Please Enter a Store</h2>
-                <input type="text" required placeholder="Store Name" />
-                <button type="submit"> Visit Store ></button>
-            </form>
-        )
-    }
+  goToStore = event => {
+    // Arrow function to bind it to Storepicker (instead of using constructor)
+    // 1. Stop the form from submitting
+    event.preventDefault();
+    // 2. Get the text from that input
+    // https://reactjs.org/docs/refs-and-the-dom.html#accessing-refs
+    const storeName = this.myInput.current.value;
+    // 3. Change the page to /store/whatever-they-entered
+    this.props.history.push(`store/${storeName}`);
+  };
+
+  render() {
+    return (
+      <form className="store-selector" onSubmit={this.goToStore}>
+        {/* This is a comment */}
+
+        <h2>Please Enter a Store</h2>
+        <input
+          type="text"
+          ref={this.myInput}
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+        />
+        <button type="submit"> Visit Store ></button>
+      </form>
+    );
+  }
 }
 
 export default StorePicker;
